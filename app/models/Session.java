0b5b04c0e141cbdca3338cc6911d031dee13754b
James Ward <james@jamesward.org>
2014-08-01 09:31:44 -0600

    fix mappings

--- app/models/Session.java
+++ app/models/Session.java
@@ -7,14 +7,13 @@ import play.db.ebean.*;
 import java.util.Date;
 
 @Entity
+@Table(schema = "salesforce", name = "session__c")
 public class Session extends Model {
 
     @Id
     public String Id;
 
     public String Name;
-
-    public Date Session_Date__c;
 
     public static Finder<Long, Session> find = new Finder<Long, Session>(Long.class, Session.class);
 
